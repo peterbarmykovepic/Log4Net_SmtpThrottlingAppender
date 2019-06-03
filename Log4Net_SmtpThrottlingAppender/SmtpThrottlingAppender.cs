6692123e366b7c0d677f737e2d1b9d60e4b3a213
Peter Barmykov <peter.barmykov@epicsoftwaredev.com>
2019-06-03 23:24:44 +0300

    initial commit

---
+++ Log4Net_SmtpThrottlingAppender/SmtpThrottlingAppender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using log4net.Appender;
+using log4net.Core;
+
+namespace Log4Net_SmtpThrottlingAppender
+{
+    public class SmtpThrottlingAppender : SmtpAppender
+    {
+        private DateTime _lastFlush = DateTime.MinValue;
+
+        public TimeSpan FlushInterval { get; set; } = new TimeSpan(0, 5, 0);
+
+        protected override void SendBuffer(LoggingEvent[] events)
+        {
+            if (DateTime.Now - _lastFlush > FlushInterval)
+            {
+                base.SendBuffer(events);
+                _lastFlush = DateTime.Now;
+            } 
+        }
+    }
+}
