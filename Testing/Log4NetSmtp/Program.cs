29863a90beebcbbb843b6add40419de5641a6968
Peter Barmykov <peter.barmykov@epicsoftwaredev.com>
2019-06-04 01:52:14 +0300

    added test projects

---
+++ Testing/Log4NetSmtp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+
+namespace Log4NetSmtp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var config = new System.IO.FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            XmlConfigurator.Configure(config);
+            var logger = LogManager.GetLogger("log");
+            
+            logger.Debug("This is a Debug message");
+            logger.Info("This is a Info message");
+            logger.Warn("This is a Warning message");
+            logger.Error("This is an Error message");
+            logger.Fatal("This is a Fatal message");
+            
+
+            for (var i = 0; i < 10; i++)
+            {
+                Thread.Sleep(2000);
+                logger.Warn("Test from test app");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Testing/NLogSmtp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogSmtp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            for (var i = 0; i < 10; i++)
+            {
+                Thread.Sleep(2000);
+                logger.Warn("Test from test app");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
